8d902ef75ac00734c08646c32c60deec867c65f9
Sean Friedmann <seanpfriedmann@gmail.com>
2022-11-27 22:28:59 -0500

    parallax initial setup

---
+++ Parallax Background/script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d'); //contains properties and draw in methods 
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 5;
+
