43c2f8220a562b35891704e8cb3cf00e9d5f406e
Sean Friedmann <seanpfriedmann@gmail.com>
2022-12-01 17:56:31 -0500

    parallax wip

--- Parallax Background/script.js
+++ Parallax Background/script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d'); //contains properties and draw in methods 
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 5;
+let gameSpeed = 10;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'layer-1.png';
@@ -15,17 +15,62 @@ backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
-let x = 0; //position of first igm
-let x2 = 2400; //position of second identical img
+const slider = document.getElementById('slider');
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById('showGameSpeed');
+showGameSpeed.innerHTML = gameSpeed;
+slider.addEventListener('change', function(event) {
+    console.log(event.target.value);
+    gameSpeed = event.target.value;
+    showGameSpeed.innerHTML = event.target.value;
+});
+
+class Layer { //used for creating similar objects
+    constructor(image, speedModifier){
+        this.x = 0; //set x on this object to 0
+        this.y - 0; 
+        this.width = 2400;
+        this.height = 700;
+       // this.x2 = this.width; //set 2nd img to keep loop
+        this.image = image; //tell constructor called img on new object to equal image we pass as arg
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier; //each layer moves at diff speed but still tied to game speed
+    }
+    update() {
+        //reset layers 
+        //wrapped in reusable class to allow us for all 5 layers
+        this.speed = gameSpeed * this.speedModifier;
+        if (this.x <= -this.width){
+            this.x = 0;
+        }
+        // if (this.x2 <= -this.width){
+        //     this.x2 = this.width + this.x - this.speed; //ensure no gaps 
+        // }
+        this.x = Math.floor(this.x - this.speed);
+        // this.x2 = Math.floor(this.x2 - this.speed);
+    }
+    draw() {
+        //redraw img at new position
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+    }
+
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.2);//half game speed
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); //clear old frames, built in method
-    ctx.drawImage(backgroundLayer4, x, 0); //built in canvas method
-    ctx.drawImage(backgroundLayer4, x2, 0);
-    if (x < -2400) x = 2400 + x2 - gameSpeed; //allows for seamless switch between canvases, no empty space based off game speed
-    else x -= gameSpeed; 
-    if (x2 < -2400) x = 2400 + x - gameSpeed;
-    else x2 -= gameSpeed;
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    });
     requestAnimationFrame(animate);
 };
 animate();
